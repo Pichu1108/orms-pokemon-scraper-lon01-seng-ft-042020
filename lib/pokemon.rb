09e19b06e11ebf03dae7f5d8f055660d33daa569
Manuel Alb Martin jukisz <manujukisz@outlook.com>
2020-05-02 19:01:47 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,7 @@
-
 class Pokemon
-  attr_accessor :id, :name, :type
-  def initialize(name, type, db)
-    @db = db
+  attr_accessor :id, :name, :type, :db
+  def initialize(id:, name:, type:)
+    @id = id
     @name = name
     @type =type
   end
